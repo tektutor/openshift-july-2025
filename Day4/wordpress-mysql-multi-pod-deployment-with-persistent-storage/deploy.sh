fcd2b5b1e310caee470758555ced43ad90bc00ba
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-07-10 12:32:13 +0530

    Added Day4 lab exercises

---
+++ Day4/wordpress-mysql-multi-pod-deployment-with-persistent-storage/deploy.sh
@@ -0,0 +1,12 @@
+echo "\nDeploying mysql ..."
+oc apply -f mysql-pv.yml
+oc apply -f mysql-pvc.yml
+oc apply -f mysql-deploy.yml
+oc apply -f mysql-svc.yml
+
+echo "\nDeploying wordpress ..."
+oc apply -f wordpress-pv.yml
+oc apply -f wordpress-pvc.yml
+oc apply -f wordpress-deploy.yml
+oc apply -f wordpress-svc.yml
+oc apply -f wordpress-route.yml
